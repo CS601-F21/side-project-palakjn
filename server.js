04d992ca3a804625d8eaa0cddeccb07e64bdb18e
palakjn <palak29jain05@gmail.com>
2021-11-12 01:15:26 -0800

    Register page design

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ app.get("/", function(req, res) {
 
 app.get("/login", function(req, res) {
     res.render("login")
-})
+});
+
+app.get("/register", function(Req, res) {
+    res.render("register")
+});
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
