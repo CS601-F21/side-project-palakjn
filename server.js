c233075b4d20ecf1a849ecf9ec0197557da45e92
palakjn <palak29jain05@gmail.com>
2021-11-13 11:57:09 -0800

    Authentication with Passport EJS

--- server.js
+++ server.js
@@ -1,12 +1,55 @@
+// require('dotenv').config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log(`connection to database established`)
+}).catch(err=>{
+    console.log(`db error ${err.message}`);
+    process.exit(-1)
+});
+
+const userSchema = new mongoose.Schema ({
+    fname: String,
+    lname: String,
+    email: String,
+    password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
     res.render("home")
@@ -16,12 +59,52 @@ app.get("/login", function(req, res) {
     res.render("login")
 });
 
-app.get("/register", function(Req, res) {
+app.post("/login", function(req, res) {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/dashboard");
+            });
+        }
+    });
+});
+
+app.get("/register", function(req, res) {
     res.render("register")
 });
 
-app.get("/dashboard", function(Req, res) {
-    res.render("dashboard")
+app.post("/register", function(req, res) {
+
+    User.register({username: req.body.username, fname: req.body.fname, lname: req.body.lname}, req.body.password, function(err, user){
+        if(err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/dashboard");
+            });
+        }
+    });
+});
+
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
+app.get("/dashboard", function(req, res) {
+    if (req.isAuthenticated()) {
+        res.render("dashboard");
+    } else {
+        res.redirect("/login");
+    }
 });
 
 app.listen(3000, function() {
