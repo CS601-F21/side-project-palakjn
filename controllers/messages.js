b2453289099c47934cfcb5b90cdc3f6883eafed9
palakjn <palak29jain05@gmail.com>
2021-12-05 19:41:42 -0800

    Displaying read messages

--- controllers/messages.js
+++ controllers/messages.js
@@ -11,13 +11,9 @@ module.exports = function(app, Client, Message) {
                 console.log("Got " + msgs.length + " messages");
 
                 for(var index in msgs) {
-
                     var clients = await Client.find({_id: {$eq: msgs[index].clientId}});
-                    console.log(clients);
 
                     if(clients.length == 1) {
-                        console.log("Got client " + clients[0].name + " information.");
-
                         messages.push({
                             "id": msgs[index]._id,
                             "clientName": clients[0].name,
@@ -26,11 +22,40 @@ module.exports = function(app, Client, Message) {
                     }
                 }
             }
-                
-            console.log(messages);
 
             res.render("messages", {
                 "read": false,
+                "messages": messages
+            })
+        } else {
+            res.redirect("/login");
+        }
+    });
+
+    app.get("/messages/read", async function(req, res) {
+        if (req.isAuthenticated()) {            
+            var messages = [];
+
+            var msgs = await Message.find({read: true});
+
+            if(msgs.length > 0) {
+                console.log("Got " + msgs.length + " messages");
+
+                for(var index in msgs) {
+                    var clients = await Client.find({_id: {$eq: msgs[index].clientId}});
+
+                    if(clients.length == 1) {
+                        messages.push({
+                            "id": msgs[index]._id,
+                            "clientName": clients[0].name,
+                            "email": clients[0].email
+                        }); 
+                    }
+                }
+            }
+
+            res.render("messages", {
+                "read": true,
                 "messages": messages
             })
         } else {
